720f9b2be88ec4ea36ddff15cb4c8c7706225028
DigitalAksumite <digitalaksumitehq@gmail.com>
2026-04-20 19:49:39 +0300

    remove node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
-
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  output: 'static',
-  adapter: node({
-    mode: 'standalone'
-  })
+  output: 'static'
 });
